d2dc935346af3fdd7c109cf762fce85f44561309
guilherme3110 <guilherme311094@hotmail.com>
2022-11-11 21:20:47 -0300

    faltando o css

--- index.js
+++ index.js
@@ -25,38 +25,68 @@ let productsCart = [
         price: 17.98,
     },
 ];
-function somaItens(productsCart){
-    for(let i = 0; i < productsCart.length; i++){
-        
-    }
-
-}
-
-productsCart()
 
 let body = document.querySelector("body")
-body.appendChild(body)
+
 
 let main = document.createElement('main')
-main.appendChild(body)
+body.appendChild(main)
 
-let liProduto = document.createElement("li")
-liProduto.classList.add(productsCart)
+let h1 = document.createElement('h1')
+h1.innerText = 'Virtual Market'
+main.appendChild(h1)
+
+let div = document.createElement('div')
+main.appendChild(div)
+
+let p = document.createElement("p")
+        p.classList = 'color'
+        p.innerText = 'item'
+
+        let span = document.createElement("span")
+        span.classList = 'colorValor'
+        span.innerText = 'valores'
+ div.append(p, span)
+
+let ul = document.createElement('ul')
+main.appendChild(ul)
+
+let soma = 0
+
+function renderCard(lista) {
+    for(let i = 0; i < lista.length; i++){
+        let liProduto = document.createElement("li")
+        liProduto.classList.add('productsCart')
 
 
-let titulo = document.createElement("p")
-titulo.classList = 'color'
-titulo.innerText = {productsCart[i].name}
+        let titulo = document.createElement("p")
+        titulo.classList = 'color'
+        titulo.innerText = lista[i].name
 
-let span = document.createElement("span")
-span.classList = 'colorValor'
-titulo.innerText = `${productsCart[i].price},00`
+        let span = document.createElement("span")
+        span.classList = 'colorValor'
+        span.innerText = `R$ ${lista[i].price}`
 
-boxItem.appendChild(p)
-boxItem.appendChild(titulo)
-boxItem.appendChild(span)
+        liProduto.append(titulo, span)
+        ul.appendChild(liProduto)
+        soma += lista[i].price
+    }
+    
+}
+renderCard(productsCart)
 
+let section  = document.createElement('section')
+main.appendChild(section)
 
+let paragrafo = document.createElement("p")
+        paragrafo.classList = 'color'
+        paragrafo.innerText = 'total'
 
+        let texto = document.createElement("span")
+        texto.classList = 'total'
+        texto.innerText = `R$ ${soma}`
+ section.append(paragrafo, texto)
 
-
+let button = document.createElement('button')
+main.appendChild(button)
+button.innerText = 'Finalizar Compra'
